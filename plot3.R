595e854aed9153c13968382a262a9f36a94e74e4
Rebecca Evans <missrlc@hotmail.com>
2015-02-08 02:47:07 +1100

    Optimised reading of file using fread

--- plot3.R
+++ plot3.R
@@ -1,19 +1,17 @@
-## read the file
-conn <- file("household_power_consumption.txt","rt")  ## open file connection
-header <- readLines(conn,n=1)  ## read the header
-lines <- readLines(conn)  ## read the remaining file
-close(conn) ## close file connection
+library(data.table)
 
-lines <- grep("^[12]/2/2007", lines, value=TRUE)  ## filter only wanted lines
-lines <- c(header,lines)  ## add the header back
+## read first column of file to determine relevant rows
+rows <- fread("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors=FALSE, colClasses=c("character",rep("NULL",8)))
+indices <- rows[,.I[Date=="1/2/2007" | Date=="2/2/2007"]]
+length(indices)
 
-## read data into a table, and convert col 1 to date
-setClass("myDate")
-setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
-dataset <- read.table(textConnection(lines), header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings="?", colClasses=c("myDate", "character", rep("numeric", 7)))
+## read data for relevant rows
+dataset <- fread("household_power_consumption.txt", sep=";", header=FALSE, skip=indices[1], nrows=length(indices), colClasses=c("character","character",rep("NULL",4), rep("numeric",3)))
+setnames(dataset,c("Date","Time","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 ## convert col 2 to DateTime
-dataset[,"Time"]<-list(strptime(paste(dataset[,"Date"],dataset[,"Time"]), "%Y-%m-%d %H:%M:%S"))
+dataset[,Date:=as.Date(dataset[,Date], format="%d/%m/%Y")]
+dataset[,Time:=as.POSIXct(strptime(paste(dataset[,Date],dataset[,Time]),"%Y-%m-%d %H:%M:%S"))]
 
 ## construct plot and save to png (480px by 480px)
 png(filename="plot3.png", width=480, height=480, units="px") ## open png device
